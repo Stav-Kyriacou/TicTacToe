6fee9fa00932371eeb392880cd1466f710a96e73
Stavros <101610510@student.swin.edu.au>
2021-04-21 18:14:23 +1000

    some comments

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace TicTacToe
             while (!victory)
             {
                 DrawGrid();
-                validInput = false;
+                validInput = false;                                     //set false to enter the input validation loop
 
                 if (player1Turn)
                 {
@@ -32,47 +32,41 @@ namespace TicTacToe
                 {
                     Console.Write("Enter a grid number: ");
 
-                    if (int.TryParse(Console.ReadLine(), out int input))                            //check if the input was a number
+                    if (int.TryParse(Console.ReadLine(), out int input))
                     {
-                        if (input > 0 && input < 10)                                                //check if number is within range
+                        if ((input > 0 && input < 10) && (grid[input - 1] != "X" && grid[input - 1] != "O"))
                         {
-                            if (grid[input - 1] != "X" && grid[input - 1] != "O")                   //check if the space is already taken
+                            if (player1Turn)
                             {
-                                if (player1Turn)                                                    //write X or O in valid place depending on turn
-                                {
-                                    grid[input - 1] = "X";
-                                    player1Turn = false;
-                                }
-                                else
-                                {
-                                    grid[input - 1] = "O";
-                                    player1Turn = true;
-                                }
-                                validInput = true;
-                                CheckVictory();
+                                grid[input - 1] = "X";
+                                player1Turn = false;
                             }
                             else
                             {
-                                Console.WriteLine("Invalid Input: Space taken");
-                                validInput = false;
+                                grid[input - 1] = "O";
+                                player1Turn = true;
                             }
+                            validInput = true;
+                            CheckVictory();
                         }
                         else
                         {
-                            Console.WriteLine("Invalid Input: Enter a number between 1 and 9");
+                            WriteInputError();
                             validInput = false;
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Invalid Input: Enter a number");
+                        WriteInputError();
                         validInput = false;
                     }
                 }
 
             }
 
+            //win con achieved
             DrawGrid();
+
             if (!player1Turn)
             {
                 Console.WriteLine("Player 1 wins!");
@@ -85,6 +79,7 @@ namespace TicTacToe
 
         public static void DrawGrid()
         {
+            //clears the console and draws the grid
             Console.Clear();
             Console.WriteLine("   |   |   ");
             Console.WriteLine(" {0} | {1} | {2} ", grid[0], grid[1], grid[2]);
@@ -98,6 +93,7 @@ namespace TicTacToe
         }
         public static void CheckVictory()
         {
+            //checks all possible victory conditions
             if ((grid[0] == "X" && grid[1] == "X" && grid[2] == "X") ||                 //horizontal X win cons
                 (grid[3] == "X" && grid[4] == "X" && grid[5] == "X") ||
                 (grid[6] == "X" && grid[7] == "X" && grid[8] == "X") ||
@@ -124,6 +120,13 @@ namespace TicTacToe
             {
                 victory = false;
             }
+        }
+        public static void WriteInputError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid Input: Enter a number between 1 and 9 that isn't already taken");
+            Console.WriteLine();
+            Console.ResetColor();
         }
     }
 }
